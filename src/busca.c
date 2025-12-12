5774e0a992211e1f1a5dff3e4cb664ac2a37efda
Geise Silva <136864987+Geisesilva@users.noreply.github.com>
2025-12-12 00:14:56 -0400

    Update busca.c
    adicionando os comentarios

--- src/busca.c
+++ src/busca.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
-#include <string.h>
+#include <string.h> //Usado para strstr
 #include "busca.h"
 #include "utils.h"
 
 void buscarPorNome(Profissional lista[], int count, const char *nome) {
     int encontrados = 0;
     for (int i = 0; i < count; i++) {
-        if (strstr(lista[i].nome, nome) != NULL) {
+        if (strstr(lista[i].nome, nome) != NULL) { //Procura nome dentro de lista[i].nome
             printf("\nID: %d\n", lista[i].id);
             printf("Nome: %s\n", lista[i].nome);
-            printf("Área: %s\n", lista[i].area);
+            printf("Area: %s\n", lista[i].area);
             printf("Contato: %s\n", lista[i].contato);
-            printf("Descrição: %s\n", lista[i].descricao);
+            printf("Descricao: %s\n", lista[i].descricao);
             printf("----------------------------------------------------\n");
             encontrados++;
         }
@@ -26,14 +26,14 @@ void buscarPorArea(Profissional lista[], int count, const char *area) {
         if (strstr(lista[i].area, area) != NULL) {
             printf("\nID: %d\n", lista[i].id);
             printf("Nome: %s\n", lista[i].nome);
-            printf("Área: %s\n", lista[i].area);
+            printf("Area: %s\n", lista[i].area);
             printf("Contato: %s\n", lista[i].contato);
-            printf("Descrição: %s\n", lista[i].descricao);
+            printf("Descricao: %s\n", lista[i].descricao);
             printf("----------------------------------------------------\n");
             encontrados++;
         }
     }
     if (encontrados == 0) {
-        printf("Nenhuma profissional encontrada nessa área.\n");
+        printf("Nenhuma profissional encontrada nessa area.\n");
     }
 }
