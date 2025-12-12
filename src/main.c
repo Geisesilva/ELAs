c73c799aae215921823c375092e017938abfcd65
Geise Silva <136864987+Geisesilva@users.noreply.github.com>
2025-12-12 00:18:34 -0400

    Update main.c
    atualizando comentario

--- src/main.c
+++ src/main.c
@@ -3,11 +3,11 @@
 #include "interface.h"
 
 int main() {
-    Profissional lista[1000];
-    int count = carregarArquivo(lista, 1000);
+    Profissional lista[1000]; //Cria um vetor com até 1000 profissionais.
+    int count = carregarArquivo(lista, 1000); //Carrega os dados já existentes.
 
-    menuPrincipal(lista, &count);
+    menuPrincipal(lista, &count); //Roda o menu.
 
-    salvarArquivo(lista, count);
+    salvarArquivo(lista, count); //Salva
     return 0;
 }
