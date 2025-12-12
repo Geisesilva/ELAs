f2c5fb886f19063ec7f6b8882191689716e25e63
Geise Silva <136864987+Geisesilva@users.noreply.github.com>
2025-12-12 00:12:37 -0400

    Comentarios
    Adicionando os comentários que ficaram no Clion

--- src/arquivo.c
+++ src/arquivo.c
@@ -1,27 +1,28 @@
 #include <stdio.h>
-#include <sys/stat.h>
+#include <sys/stat.h> //Biblioteca que permite criar pastas e mexer com permissões de arquivos no Linux.
 #include <strings.h>
 #include "arquivo.h"
 
-static const char *CAMINHO = "data/elas.bin";
+static const char *CAMINHO = "data/elas.bin"; //Define uma constante que guarda o caminho do arquivo binário.
 
-static void garantirPasta() {
-#ifdef _WIN32
-    mkdir("data");
+static void garantirPasta() { //Serve para criar a pasta /data.
+#ifdef _WIN32 //Para compilar no Windows.
+    mkdir("data"); //cria a pasta.
 #else
-    mkdir("data", 0777);
+    mkdir("data", 0777); //para Linux e MacOS.
 #endif
 }
 
-int carregarArquivo(Profissional lista[], int max) {
+int carregarArquivo(Profissional lista[], int max) { 
     garantirPasta();
 
-    FILE *f = fopen(CAMINHO, "rb");
+    FILE *f = fopen(CAMINHO, "rb"); //Abre o arquivo binário
     if (!f) {
-        return 0; // arquivo ainda não existe, lista vazia
+        return 0; //se o arquivo ainda não existe, lista vazia
     }
 
-    int count = fread(lista, sizeof(Profissional), max, f);
+    int count = fread(lista, sizeof(Profissional), max, f); /*lê vários registros de uma vez e
+    coloca dentro do array lista.*/
     fclose(f);
     return count;
 }
@@ -29,12 +30,12 @@ int carregarArquivo(Profissional lista[], int max) {
 void salvarArquivo(Profissional lista[], int count) {
     garantirPasta();
 
-    FILE *f = fopen(CAMINHO, "wb");
+    FILE *f = fopen(CAMINHO, "wb"); //Abre o arquivo binário para escrita.
     if (!f) {
         printf("Erro ao salvar arquivo.\n");
         return;
     }
 
-    fwrite(lista, sizeof(Profissional), count, f);
+    fwrite(lista, sizeof(Profissional), count, f); // Escreve TODOS os registros no arquivo de uma só vez.
     fclose(f);
 }
